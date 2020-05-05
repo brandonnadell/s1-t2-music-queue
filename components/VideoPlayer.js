ee3c43943c4f73f1cd2a97e3db13da1774a38423
Brandon Nadell <brandonnadell@gmail.com>
2020-05-05 14:39:27 -0700

    bn - added video progress bar

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,10 +1,17 @@
 import ReactPlayer from "react-player";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
+import ProgressBar from "react-bootstrap/ProgressBar";
 
-export class VideoPlayer extends React.Component {
-  url = "";
-  playing = true;
-  muted = false;
-  player = null;
+export function VideoPlayer(props) {
+  let url = props.url;
+  let muted = props.muted;
+  const [player, setPlayer] = useState("");
+  const [playing, setPlaying] = useState("");
+  const [progress, setProgress] = useState("");
+  const [started, setStarted] = useState("");
 
   setVideo = (url) => {
     this.url = url;
@@ -12,6 +19,7 @@ export class VideoPlayer extends React.Component {
     this.muted = false;
   };
 
+<<<<<<< HEAD
   stop = () => {
     this.player.getInternalPlayer().stopVideo();
   };
@@ -19,11 +27,27 @@ export class VideoPlayer extends React.Component {
   ref = (player) => {
     this.player = player;
   };
+=======
+  function handleStart() {
+    setPlaying(true);
+    setStarted(true);
+  }
+
+  function handleReady() {
+    setStarted(false);
+    setProgress(0);
+  }
+
+  function handleProgress() {
+    setProgress((player.getCurrentTime() / player.getDuration()) * 100);
+  }
+>>>>>>> bn - added video progress bar
 
   getPlayer() {
     return player;
   }
 
+<<<<<<< HEAD
   render() {
     //Video with playlist embedded
     // this.setVideo("https://www.youtube.com/watch?v=KpvvjoD9CSs&list=PLxOWk5x66lsHCeuqC86VRNko3U7kw8VMh&index=2&t=0s");
@@ -42,10 +66,47 @@ export class VideoPlayer extends React.Component {
       // this.player.getInternalPlayer().loadVideoByUrl("https://www.youtube.com/v/zTitoHKsyJg?version=3");
     }
     return (
+=======
+  return (
+    <div>
+      {url != "" ? (
+        <div>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={props.img} />
+            {started ? (
+              <Card.Body>
+                <div>
+                  <div>
+                    <Button
+                      className="mb-3"
+                      variant="outline-primary"
+                      onClick={handleToggle}
+                    >
+                      {playing ? "Pause" : "Play"}
+                    </Button>
+                  </div>
+                  <div>
+                    {playing ? "Playing" : "Paused"}: {props.videoTitle}
+                  </div>
+                  <div>
+                    <ProgressBar now={progress} />
+                  </div>
+                </div>
+              </Card.Body>
+            ) : (
+              <div></div>
+            )}
+          </Card>
+        </div>
+      ) : (
+        <div></div>
+      )}
+>>>>>>> bn - added video progress bar
       <ReactPlayer
         url={this.url}
         controls={true}
         pip={false}
+<<<<<<< HEAD
         playing={this.playing}
         muted={this.muted}
         ref={this.ref}
@@ -56,6 +117,21 @@ export class VideoPlayer extends React.Component {
         //         }
         //     }
         //   }}
+=======
+        playing={true}
+        muted={muted}
+        width={0}
+        height={0}
+        ref={initPlayer}
+        onStart={handleStart}
+        onProgress={handleProgress}
+        onReady={handleReady}
+        config={{
+          youtube: {
+            playerVars: { disablekb: 1, autoplay: 1 },
+          },
+        }}
+>>>>>>> bn - added video progress bar
       />
     );
   }
