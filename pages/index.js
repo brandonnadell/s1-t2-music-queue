6c2462f465d166d066a5db2bd36efd436ae36fdd
Yuval Steinhart <yuval@cs.ucsb.edu>
2020-05-05 21:22:55 -0700

    ys - moved youtube components to room page

--- pages/index.js
+++ pages/index.js
@@ -8,36 +8,16 @@ export const getServerSideProps = optionalAuth;
 
 function HomePage(props) {
   const user = props.user;
-  const [queue, setQueue] = useState([]);
-  const [titles, setTitles] = useState([]);
-  const [imgs, setImgs] = useState([]);
-
-  function handleUrlClick(vidUrl, vidTitle, vidImg) {
-    setQueue(queue.concat(vidUrl));
-    setTitles(titles.concat(vidTitle));
-    setImgs(imgs.concat(vidImg));
-  }
 
   return (
     <Layout user={user}>
       {user ? (
         <div>
-          <div>
-            <SearchBar onUrlClick={handleUrlClick} />
-          </div>
-          <div>
-            <VideoPlayer
-              queue={queue}
-              titles={titles}
-              imgs={imgs}
-              muted={false}
-            />
-          </div>
           <CreateRoom user={user}></CreateRoom>
         </div>
       ) : (
-          <div>You're not logged in!</div>
-        )}
+        <div>You're not logged in!</div>
+      )}
     </Layout>
   );
 }
--- pages/room/[roomid].js
+++ pages/room/[roomid].js
@@ -1,21 +1,45 @@
 import { useRouter } from "next/router";
 import AppNavBar from "../../components/AppNavbar";
 import AppFooter from "../../components/AppFooter";
+import SearchBar from "../../components/SearchBar";
+import VideoPlayer from "../../components/VideoPlayer";
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout";
 import { requiredAuth } from "../../utils/ssr";
 
 export const getServerSideProps = requiredAuth;
 
 const Room = (props) => {
-  const user = props.user;
   const router = useRouter();
   const { roomid } = router.query;
+  const [queue, setQueue] = useState([]);
+  const [titles, setTitles] = useState([]);
+  const [imgs, setImgs] = useState([]);
+  const user = props.user;
+
+  function handleUrlClick(vidUrl, vidTitle, vidImg) {
+    setQueue(queue.concat(vidUrl));
+    setTitles(titles.concat(vidTitle));
+    setImgs(imgs.concat(vidImg));
+  }
 
   return (
-    <>
-      <AppNavBar user={user} />
+    <Layout user={user}>
       <p>Room: {roomid}</p>
-      <AppFooter />
-    </>
+      <div>
+        <div>
+          <SearchBar onUrlClick={handleUrlClick} />
+        </div>
+        <div>
+          <VideoPlayer
+            queue={queue}
+            titles={titles}
+            imgs={imgs}
+            muted={false}
+          />
+        </div>
+      </div>
+    </Layout>
   );
 };
 
