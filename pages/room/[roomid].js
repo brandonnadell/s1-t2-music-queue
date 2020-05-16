823c1ceebf866956d4a543c1d4d7f2022b44a68d
Yuval Steinhart <yuval@cs.ucsb.edu>
2020-05-15 18:35:19 -0700

    ys - added disconnection functionality and exception handling for invalid roomids

--- pages/room/[roomid].js
+++ pages/room/[roomid].js
@@ -7,6 +7,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import { requiredAuth } from "../../utils/ssr";
 import firebase from "../../helpers/firebase";
+import Button from "react-bootstrap/Button";
 
 export const getServerSideProps = requiredAuth;
 
@@ -18,43 +19,75 @@ const Room = (props) => {
   let userid;
   let firebaseRef;
   let roomRef;
+  let creator;
 
   useEffect(() => {
-    firebase
-      .database()
-      .ref("rooms")
-      .child(roomid)
-      .once("value")
-      .then((res) => {
-        if (!res.hasChild("creator")) {
-          router.push("/");
-          firebase
-            .database()
-            .ref("rooms/" + roomid)
-            .remove();
-        }
+    try {
+      firebase
+        .database()
+        .ref("rooms")
+        .child(roomid)
+        .once("value")
+        .then((res) => {
+          if (!res.hasChild("creator")) {
+            router.push("/");
+            firebase
+              .database()
+              .ref("rooms/" + roomid)
+              .remove();
+          } else {
+            roomRef.on("value", (snapshot) => {
+              creator = snapshot.val().creator;
+            });
+          }
+        });
+
+      roomRef = firebase.database().ref("rooms/" + roomid);
+      userid = roomRef.child("/users").push(user).key;
+      firebaseRef = firebase.database().ref("rooms/" + roomid + "/songs");
+      firebaseRef.on("value", (snapshot) => {
+        // Grab the current value of what was written to the Realtime Database.
+        const tempList = [];
+        snapshot.forEach((child) => {
+          tempList.push({ key: child.key, val: child.val() });
+        });
+        setList(tempList);
+      });
+      window.addEventListener("beforeunload", function (event) {
+        roomRef.child("users/" + userid).remove();
+        roomRef.once("value").then((res) => {
+          if (res.hasChild("users")) {
+            if (creator == user.nickname) {
+              roomRef
+                .child("users")
+                .once("value")
+                .then((snapshot) => {
+                  let list = [];
+                  snapshot.forEach((child) => {
+                    list.push(child.val().nickname);
+                  });
+                  const tmp = list[0];
+                  roomRef.child("creator").set(tmp);
+                  creator = tmp;
+                  console.log(tmp);
+
+                  console.log(creator);
+                });
+            }
+          } else {
+            roomRef.child("creator").set(null);
+            roomRef.remove();
+          }
+        });
       });
 
-    roomRef = firebase.database().ref("rooms/" + roomid);
-
-    userid = roomRef.child("/users").push(user).key;
-    firebaseRef = firebase.database().ref("rooms/" + roomid + "/songs");
-    firebaseRef.on("value", (snapshot) => {
-      // Grab the current value of what was written to the Realtime Database.
-      const tempList = [];
-      snapshot.forEach((child) => {
-        tempList.push({ key: child.key, val: child.val() });
-      });
-      setList(tempList);
-    });
-    window.addEventListener("beforeunload", function (event) {
-      roomRef.child("users/" + userid).remove();
-    });
-
-    // return a cleanup function that will get called by React on unmount
-    return () => {
-      firebaseRef.off();
-    };
+      // return a cleanup function that will get called by React on unmount
+      return () => {
+        firebaseRef.off();
+      };
+    } catch (err) {
+      router.push("/");
+    }
   }, []);
 
   return (
